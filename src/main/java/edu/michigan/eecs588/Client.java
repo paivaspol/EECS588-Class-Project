ab53b2ddac3229867206856f2a27d28aaf327eea
Eugene Goh <vanblaze5@gmail.com>
2015-04-12 15:11:52 -0400

    Finished messaging prompts

--- src/main/java/edu/michigan/eecs588/Client.java
+++ src/main/java/edu/michigan/eecs588/Client.java
@@ -10,12 +10,15 @@ import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jivesoftware.smack.util.StringUtils;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 import org.jivesoftware.smackx.muc.MultiUserChatManager;
 import org.jivesoftware.smackx.xdata.Form;
 import org.jivesoftware.smackx.xdata.FormField;
+import org.jxmpp.util.XmppStringUtils;
 
 /**
  * Entry point for the client for the XMPP.
@@ -29,34 +32,36 @@ public class Client {
 		MultiUserChatManager manager = MultiUserChatManager.getInstanceFor(connection);
 		MultiUserChat muc = manager.getMultiUserChat("myroom@conference.vaspol");
 //		System.out.println(Arrays.toString(manager.getHostedRooms("vaspol").toArray()));
-		muc.createOrJoin("FirstRoom");
+		muc.createOrJoin("vanblaze");
 		// Get the the room's configuration form
-		Form form = muc.getConfigurationForm();
+//		Form form = muc.getConfigurationForm();
 		// Create a new form to submit based on the original form
-		Form submitForm = form.createAnswerForm();
+//		Form submitForm = form.createAnswerForm();
 		// Add default answers to the form to submit
-		for (FormField field : form.getFields()) {
-			if (!FormField.Type.hidden.equals(field.getType()) && field.getVariable() != null) {
+//		for (FormField field : form.getFields()) {
+//			if (!FormField.Type.hidden.equals(field.getType()) && field.getVariable() != null) {
 				// Sets the default value as the answer
-				submitForm.setDefaultAnswer(field.getVariable());
-			}
-		}
+//				submitForm.setDefaultAnswer(field.getVariable());
+//			}
+//		}
 		// Sets the new owner of the room
-		List<String> owners = new ArrayList<>();
-		owners.add("a@vaspol");
+
 //		submitForm.setAnswer("muc#roomconfig_roomowners", owners);
 		// Send the completed form (with default values) to the server to configure the room
 //		muc.sendConfigurationForm(submitForm);
-		System.out.println(muc.toString());
+//		System.out.println(muc.toString());
+		System.out.println("Welcome to " + muc.getRoom());
 		System.out.println("Type your message and press Enter to send.");
 		while (true) {
 			input = in.nextLine();
 			muc.sendMessage(input);
-			System.out.println(muc.nextMessage().getBody());
+
+			Message message = muc.nextMessage();
+			System.out.println(XmppStringUtils.parseResource(message.getFrom()) + ": " + message.getBody());
 		}
     }
     
-    /**
+    /** 35.2.91.44
      * Connects to the xmpp server.
      * 
      * @throws SmackException
@@ -67,14 +72,14 @@ public class Client {
 		XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
 				.setServiceName("vaspol")
 				.setUsernameAndPassword("a", "password")
-				.setHost("localhost")
+				.setHost("35.2.91.44")
 				.setPort(5222)
 				.setSecurityMode(SecurityMode.disabled)
 				.build();
 		AbstractXMPPConnection connection = new XMPPTCPConnection(config);
 		connection.connect();
-		connection.login("a", "password");
-		System.out.println("Connected to XMPP server!");
+		connection.login("eugene", "password");
+		System.out.println("Connected to XMPP server " + connection.getHost() + "!");
     	return connection;
     }
 }
