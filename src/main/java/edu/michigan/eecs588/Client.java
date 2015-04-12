d2793aeaf57e2e8c4d70b3a04a3c47bb5c676ba8
Eugene Goh <vanblaze5@gmail.com>
2015-04-12 10:25:40 -0400

    Updated gitignore, Get client to a working state

--- src/main/java/edu/michigan/eecs588/Client.java
+++ src/main/java/edu/michigan/eecs588/Client.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
 import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 import org.jivesoftware.smackx.muc.MultiUserChatManager;
+import org.jivesoftware.smackx.muc.RoomInfo;
+import org.jivesoftware.smackx.xdata.Form;
+import org.jivesoftware.smackx.xdata.packet.DataForm;
+
 
 /**
  * Entry point for the client for the XMPP.
@@ -18,9 +23,9 @@ public class Client {
 	
     public static void main(String[] args) throws SmackException, IOException, XMPPException {
         AbstractXMPPConnection connection = createConnection();
-        MultiUserChatManager manager = MultiUserChatManager.getInstanceFor(connection);
-        MultiUserChat muc = manager.getMultiUserChat("test@conference.vaspol");
-        muc.create("test");
+		MultiUserChatManager manager = MultiUserChatManager.getInstanceFor(connection);
+		MultiUserChat muc = manager.getMultiUserChat("test@example.com");
+		System.out.println(muc.toString());
         while (true) {}
     }
     
@@ -32,16 +37,26 @@ public class Client {
      * @throws XMPPException
      */
     private static AbstractXMPPConnection createConnection() throws SmackException, IOException, XMPPException {
-    	XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
-    			  .setUsernameAndPassword("d", "password")
-    			  .setServiceName("vaspol")
-    			  .setHost("localhost")
-    			  .setPort(5222)
-    			  .setSecurityMode(SecurityMode.disabled)
-    			  .build();
-    	AbstractXMPPConnection connection = new XMPPTCPConnection(config);
-    	connection.connect();
-    	System.out.println("Connected to XMPP server!");
+		XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
+				.setServiceName("example.com")
+				.setUsernameAndPassword("me", "1234")
+				.setHost("localhost")
+				.setPort(5222)
+				.setSecurityMode(SecurityMode.disabled)
+				.build();
+		AbstractXMPPConnection connection = new XMPPTCPConnection(config);
+		try {
+			connection.connect();
+			connection.login("me", "1234");
+			System.out.println("Connected to XMPP server!");
+		} catch (SmackException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (XMPPException e) {
+			e.printStackTrace();
+		}
+
     	return connection;
     }
 }
