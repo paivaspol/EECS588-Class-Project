f8fdc775fc0b18546df8e0bf83b4dc2e3b887238
Sofwan Haddad <sysofwan@umich.edu>
2015-04-12 15:45:47 -0400

    messenger class

---
+++ src/main/java/edu/michigan/eecs588/MessageReceived.java
@@ -0,0 +1,8 @@
+package edu.michigan.eecs588;
+
+/**
+ * Created by sysofwan on 4/12/15.
+ */
+public interface MessageReceived {
+    void onMessageReceived(String message);
+}
--- src/main/java/edu/michigan/eecs588/Messenger.java
+++ src/main/java/edu/michigan/eecs588/Messenger.java
@@ -1,11 +1,39 @@
 package edu.michigan.eecs588;
 
+import edu.michigan.eecs588.encryption.AESCrypto;
+import edu.michigan.eecs588.encryption.Verifier;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+
+import java.security.PublicKey;
+import java.util.Map;
 
 /**
  * Created by sysofwan on 4/12/15.
  */
 public class Messenger {
 
-    private String 
+    AESCrypto crypto;
+    MultiUserChat muc;
+    MessageReceived cb;
+    Map<String, Verifier> publicKeys;
+
+    public Messenger(MultiUserChat muc, MessageReceived callback, Map<String, Verifier> publicKeys) {
+        this.muc = muc;
+        this.cb = callback;
+        this.publicKeys = publicKeys;
+
+        muc.addMessageListener(new MessageListener() {
+            @Override
+            public void processMessage(Message message) {
+                String signature = message.getFrom();
+                signature.substring()
+                String decrypted = crypto.decrypt(message.getBody());
+                cb.onMessageReceived(decrypted);
+            }
+        });
+    }
+
+
 }
