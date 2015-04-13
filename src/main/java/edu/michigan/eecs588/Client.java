eeeb83cd39298c4ceda7fa550bfadbae5b8240eb
Vaspol Ruamviboonsuk <paivaspol@gmail.com>
2015-04-12 22:33:12 -0400

    Fixed the key-pair mismatch

--- src/main/java/edu/michigan/eecs588/Client.java
+++ src/main/java/edu/michigan/eecs588/Client.java
@@ -43,9 +43,34 @@ public class Client {
 	private AbstractXMPPConnection connection;
 	private Messenger messenger;
 	
+	private String privateKey = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC4eYiG8Atjnu6ePk3vZn7yZR7U" +
+								"Om9kY5hQtQYutLFYJoeZ5ivffhE+N/9xm0fp9xZ8FquRFaZGVcau3mjstyQL456oOgQ2BJ0h7lO3" +
+								"JN0khMd/LtXw8rrn/Im5EV+qoQpNWNv9W8f6+yTm7BanSwosMhiWF3ie/UKjqt+jngYKYFnhwqGD" +
+								"cDc3j+UB6RDerXDlQloTSoVSS3kNPZ3bhq377fEzHOuYntXT9/4H+OlnyfMmyQk95QT/m5h/B/Oh" +
+								"vI4/w9MkXt49TmuPLS7f6ihnQYJ8o6S2BThlbedbqLXheJVFIP0L1978ct4A3NWMCH7ClpVNm62c" +
+								"6FDJNZadajJxAgMBAAECggEASDYawWd5rddq5LrS2yGuE8iNltoA/LUXzI/wCZxlx3Hdptq41dWN" +
+								"fmOBNMFqFyXHwW9GXZax7dpp2c3qGK9gBt9lHckIGPUZZUzbrFdb0Y3AYgK9cIIzs9fhOXaZkjtT" +
+								"ww9DnhutXous2FAWVgpDwyUhBq/NYb8VtOeQf4W+K5T9QdU+jaOWGw/+LgmuNeOgMrx9Z9Uma+JN" +
+								"7E++o17P4EIdDE+J4yuHAlonZGRshzXGZ8lPM42YDm8htoB6dGSQryvRBrhx7IV2LY6/91pKUiGU" +
+								"0KiGfnMt7yhhyZ+6TXs/wLA30Z8xGZGrFkqxbGzUSYmoZPlijeYrIOw3P4K2eQKBgQDp5NldaGWy" +
+								"0LPn6d+u6EBT2o5kaS30t9Q8O8IhTe4RuwHTr+0yRFEEtH93mcCqDoJk4K7rh+ae+KKQyfQyv1F6" +
+								"ZUPZjIc0WQxyKh6r1mixk02EbR2Xu17h321iApLil7NUAmgwFBIeilImQ/T7DZOF04epyPz6Uf6M" +
+								"9ZBvNgo+cwKBgQDJ6PffEeYi8N4DD52lvnIpc7buk4OBU0TJMi6E9tbySAwiekye+lHTnm5t0Sci" +
+								"ZLt5ny1OQoeDYpFTz3rEP6bCyYK/RVJg2yShDQh4KLeiF6vVzZv5EkNiBA2QXVxv59yNrhh+ZSvt" +
+								"0o6FAV0Mk7/eBuN46dk92PddsPb/49AOiwKBgQDLOCmcPQ7PtQH7aHsPT4BE20qI99IUJHzk8M8V" +
+								"fM5y3VcTBHJFEJKMUf/GfPKnTwAAgi+5OVUpnsIwpyfjx9MVU1MGfFElLJHhx7LqftpsH55khyIF" +
+								"Kamg+y3g0HerJT+MqKK501aC7o+966G5V+xrhIUFAjq7i+5trxcVaZCw/QKBgBig3CtPiZzVeJ8y" +
+								"9m3TLyWzQasUP0Q1HfzUrCv31/wxoZlLBbGWowiHGL04d+eQFfYd3m7fWpxCF6v7cOQjR3oXDmW6" +
+								"VPQPhwZGCrRtcwjIjmND8zSMb7+y8tybJr1XsOVvSPmR6avWtR+wLONt2keA25pSG/eZUYMSJO7N" +
+								"oQFFAoGBAJ1zwzrRJp2uMbVM9V6cNe1EYGaMviKJzYadBbyA3a8JaKYBm2nctyBTGsm5yoZJd+6B" +
+								"NIe6GoVmp9u9GpQvdcL4QnG2JJ86NyqojUTh1XC5Mc903dRxPkcyydssxyE0dxldkKeo2mpvVBMq" +
+								"gDvVTQPycQJgFgwKiKocFzLApdY5" ;		
+			
+	private String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuHmIhvALY57unj5N72Z+8mUe1DpvZGOYULUGLrSxWCaHmeYr334RPjf/cZtH6fcWfBarkRWmRlXGrt5o7LckC+OeqDoENgSdIe5TtyTdJITHfy7V8PK65/yJuRFfqqEKTVjb/VvH+vsk5uwWp0sKLDIYlhd4nv1Co6rfo54GCmBZ4cKhg3A3N4/lAekQ3q1w5UJaE0qFUkt5DT2d24at++3xMxzrmJ7V0/f+B/jpZ8nzJskJPeUE/5uYfwfzobyOP8PTJF7ePU5rjy0u3+ooZ0GCfKOktgU4ZW3nW6i14XiVRSD9C9fe/HLeANzVjAh+wpaVTZutnOhQyTWWnWoycQIDAQAB"; 
+	
 	/* For testing purposes. */
 	Map<String, Verifier> publicKeys = new HashMap<String, Verifier>();
-	RSAKeyPair X = new RSAKeyPair();
+	RSAKeyPair X = new RSAKeyPair(privateKey, publicKey);
 	Signer sign = new Signer(X.getPrivateKey());
     
 	/**
